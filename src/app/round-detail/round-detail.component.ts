040c53e1544147760e684105b01de35de239ba1d
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2019-10-14 17:13:49 +0900

    V0.1.6 (#6)
    * fixed record of The 2014 Japan Open
    * fixed record of The 20th Nippon Open
    * rmoved records except Japan from course rating data
    
    * added an equation into round detail
    * added score-to-rating & rating-to-score functions
    
    * added a size 5 to paginator of ratings
    * improved css styles
    * removed a useless command

--- src/app/round-detail/round-detail.component.ts
+++ src/app/round-detail/round-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, Input } from '@angular/core';
 
 import { CourseRatingsItem } from '../course-rating';
 
+const MIN_RATING = 700;
+const MAX_RATING = 1200;
+
 @Component({
   selector: 'app-round-detail',
   templateUrl: './round-detail.component.html',
@@ -11,4 +14,58 @@ export class RoundDetailComponent {
 
   @Input() detail: CourseRatingsItem;
 
+  rating: number;
+  score: number;
+
+  onRatingChanged() {
+    if (typeof this.rating !== 'number') {
+      this.score = NaN;
+      return;
+    }
+    if (this.rating < MIN_RATING) {
+      this.score = NaN;
+      return;
+    }
+    if (this.rating > MAX_RATING) {
+      this.score = NaN;
+      return;
+    }
+    this.rating = Math.round(this.rating);
+    this.score = this.rating2score(this.rating);
+  }
+
+  onScoreChanged() {
+    if (typeof this.score !== 'number') {
+      this.rating = NaN;
+      return;
+    }
+    if (this.score < this.getMinScore()) {
+      this.rating = NaN;
+      return;
+    }
+    if (this.score > this.getMaxScore()) {
+      this.rating = NaN;
+      return;
+    }
+    this.score = Math.round(this.score);
+    this.rating = this.score2rating(this.score);
+  }
+
+  private rating2score(rating: number) {
+    let score = (rating - this.detail.offset) / this.detail.weight;
+    score = Math.round(score * 10) / 10;
+    return score;
+  }
+
+  private score2rating(score: number) {
+    return Math.round(score * this.detail.weight + this.detail.offset);
+  }
+
+  private getMinScore() {
+    return (this.detail.holes || 18) * 2;
+  }
+
+  private getMaxScore() {
+    return (this.detail.holes || 18) * 6;
+  }
 }
