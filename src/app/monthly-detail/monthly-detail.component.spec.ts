336d84ab311fe75c89ab1fae979d00436ca6ac2a
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2019-11-11 19:12:01 +0900

    did ng test

--- src/app/monthly-detail/monthly-detail.component.spec.ts
+++ src/app/monthly-detail/monthly-detail.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MonthlyDetailComponent } from './monthly-detail.component';
 
@@ -8,7 +10,11 @@ describe('MonthlyDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MonthlyDetailComponent ]
+      declarations: [ MonthlyDetailComponent ],
+      imports: [
+        RouterTestingModule,
+        MatIconModule,
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +22,24 @@ describe('MonthlyDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MonthlyDetailComponent);
     component = fixture.componentInstance;
+    component.event = {
+      title: 'Ryugasaki',
+      location: 'Furusato Fureai Park',
+      schedule: {
+        byDay: ['su'],
+        bySetPos: 1,
+        byMonth: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+      },
+      urls: [{
+        type: 'website',
+        title: 'Discgolf Ryugasaki',
+        url: 'https://discgolf.exblog.jp'
+      }, {
+        type: 'website',
+        title: 'Facebook',
+        url: 'https://www.facebook.com/groups/237882656402120/'
+      }]
+    };
     fixture.detectChanges();
   });
 
--- src/app/monthly-events/monthly-events.component.spec.ts
+++ src/app/monthly-events/monthly-events.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { MonthlyEventsComponent } from './monthly-events.component';
+import { MonthlyDetailComponent } from '../monthly-detail/monthly-detail.component';
 
 describe('MonthlyEventsComponent', () => {
   let component: MonthlyEventsComponent;
@@ -8,7 +17,19 @@ describe('MonthlyEventsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MonthlyEventsComponent ]
+      declarations: [
+        MonthlyEventsComponent,
+        MonthlyDetailComponent,
+      ],
+      imports: [
+        RouterTestingModule,
+        NoopAnimationsModule,
+        MatIconModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+        MatTabsModule,
+      ]
     })
     .compileComponents();
   }));
