81be45883eebd3117a04437bedbe02b2157700b8
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2019-11-26 04:53:52 +0900

    update links to local images for github pages

--- src/app/about-this-site/about-this-site.component.ts
+++ src/app/about-this-site/about-this-site.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonService } from '../common.service';
 
-import { environment } from '../../environments/environment';
-
 @Component({
   selector: 'app-about-this-site',
   templateUrl: './about-this-site.component.html',
@@ -12,10 +10,6 @@ export class AboutThisSiteComponent {
 
   get primaryLanguage() {
     return this.cs.primaryLanguage;
-  }
-
-  get baseUrl() {
-    return environment.start_url;
   }
 
   constructor(private cs: CommonService) {
--- src/environments/environment.travis.ts
+++ src/environments/environment.travis.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  start_url: '/',
   MAP_API_KEY: 'xxxx',
   map: {
     center: {
