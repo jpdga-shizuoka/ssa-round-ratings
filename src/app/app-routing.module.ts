b2e908891a0567f54382c5cc90e0be7c4f346f04
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2019-11-26 17:57:42 +0900

    V0.2.2 (#13)
    * add robots.txt and sitemap.xml
    * add ogp meta data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,46 @@ import { AboutThisSiteComponent } from './about-this-site/about-this-site.compon
 
 const routes: Routes = [
   { path: '', redirectTo: '/events/upcoming', pathMatch: 'full' },
-  { path: 'events/upcoming', component: EventsTabsComponent },
-  { path: 'events/local', component: EventsTabsComponent },
-  { path: 'events/monthly', component: EventsTabsComponent },
-  { path: 'past', component: RoundsTabsComponent },
-  { path: 'about', component: AboutThisSiteComponent },
+  { path: 'events/upcoming', component: EventsTabsComponent, data: {
+    title: 'DG Japan - Offcial Tournaments',
+    description: 'Official schedule of the disc golf events held in Japan.',
+    keywords: 'disc golf,official tournaments,japan',
+    url: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/events/upcoming',
+    image: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/assets/img/DGJAPAN.png',
+    type: 'website',
+  }},
+  { path: 'events/local', component: EventsTabsComponent, data: {
+    title: 'DG Japan - Local Events',
+    description: 'Schedule of the local events held in Japan.',
+    keywords: 'disc golf,local events,japan',
+    url: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/events/local',
+    image: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/assets/img/DGJAPAN.png',
+    type: 'article',
+  }},
+  { path: 'events/monthly', component: EventsTabsComponent, data: {
+    title: 'DG Japan - Monthly Events',
+    description: 'Schedule of the local monthly events held in Japan.',
+    keywords: 'disc golf,monthly events,japan',
+    url: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/events/monthly',
+    image: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/assets/img/DGJAPAN.png',
+    type: 'article',
+  }},
+  { path: 'past', component: RoundsTabsComponent, data: {
+    title: 'DG Japan - Results',
+    description: 'Results of the events held in Japan.',
+    keywords: 'disc golf,results,japan',
+    url: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/past',
+    image: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/assets/img/DGJAPAN.png',
+    type: 'article',
+  }},
+  { path: 'about', component: AboutThisSiteComponent, data: {
+    title: 'DG Japan - About Us',
+    description: 'About Us, Security Policy, Terms, Open Source Software Project.',
+    keywords: 'disc golf,ssa,scratch scoring averages,japan',
+    url: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/about',
+    image: 'https://jpdga-shizuoka.github.io/ssa-round-ratings/assets/img/DGJAPAN.png',
+    type: 'article',
+  }},
 ];
 
 @NgModule({
