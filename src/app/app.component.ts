b4a2b1657ef464290d8fc7d89282f0ce3d4639e9
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2019-11-26 17:14:06 +0900

    add ogp meta data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,45 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 import { MatSidenav } from '@angular/material/sidenav';
+
+import { Subscription } from 'rxjs';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 import { CommonService } from './common.service';
 import { Observable, isHandset } from './utilities';
+
+interface MyMetaData {
+  title: string;
+  type: string;
+  url: string;
+  image: string;
+  description?: string;
+  keywords?: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   @ViewChild('drawer', {static: false}) drawer: MatSidenav;
   title = 'DG Japan';
   isHandset$: Observable<boolean>;
+  subscription: Subscription;
 
   constructor(
     private cs: CommonService,
+    private route: ActivatedRoute,
+    private titleService: Title,
+    private meta: Meta,
+    public router: Router,
     breakpointObserver: BreakpointObserver,
   ) {
     this.isHandset$ = isHandset(breakpointObserver);
   }
-
 
   get upcomingEvents() {
     return this.cs.getMenuAliase('Upcoming Events');
@@ -46,6 +64,25 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     const language = window.navigator.language.split('-')[0];
     this.cs.primaryLanguage = language === 'ja' ? false : true;
+
+    this.subscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.route),
+      map(route => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      filter(route => route.outlet === 'primary'),
+      mergeMap(route => route.data)
+    ).subscribe((data: MyMetaData) => this.updateDescription(data));
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   onClickLink() {
@@ -56,5 +93,21 @@ export class AppComponent implements OnInit {
 
   onClickLanguage() {
     this.cs.toggleLanguage();
+  }
+
+  private updateDescription(data: MyMetaData) {
+    this.titleService.setTitle(data.title);
+    this.meta.updateTag({ name: 'description', content: data.description });
+    this.meta.updateTag({ property: 'og:title', content: data.title });
+    this.meta.updateTag({ property: 'og:type', content: data.type });
+    this.meta.updateTag({ property: 'og:url', content: data.url });
+    this.meta.updateTag({ property: 'og:image', content: data.image });
+
+    if (data.keywords) {
+      this.meta.updateTag({ name: 'keywords', content: data.keywords });
+    }
+    if (data.description) {
+      this.meta.updateTag({ property: 'og:description', content: data.description });
+    }
   }
 }
