c55813402ac5023ffb68dd7ae787bf21a098c167
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2021-06-29 07:14:18 +0900

    fix compiled errors

--- src/app/dialogs/marker-dialog.component.ts
+++ src/app/dialogs/marker-dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { EventCategory } from '../models';
 import { RemoteService, LocationInfo } from '../remote.service';
 
@@ -18,18 +19,14 @@ export interface MarkerDialogData {
   templateUrl: './marker-dialog.component.html',
   styleUrls: ['./marker-dialog.component.css']
 })
-export class MarkerDialogComponent implements OnInit {
-  public dialogRef: MatDialogRef<MarkerDialogComponent>;
-  location: LocationInfo;
+export class MarkerDialogComponent {
+  location$: Observable<LocationInfo>;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: MarkerDialogData,
     private readonly remote: RemoteService
-  ) {}
-
-  ngOnInit(): void {
-    this.remote.getLocation(this.data.location)
-      .subscribe(location => { this.location = location; });
+  ) {
+    this.location$ = this.remote.getLocation(this.data.location);
   }
 
   get showEventBotton(): boolean {
