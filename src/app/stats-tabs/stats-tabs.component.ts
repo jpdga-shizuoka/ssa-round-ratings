ec5e9038d55ff9b36ff559a611310c4c321f385d
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2021-07-26 21:29:48 +0900

    add difficulty chart

--- src/app/stats-tabs/stats-tabs.component.ts
+++ src/app/stats-tabs/stats-tabs.component.ts
@@ -1,35 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-stats-tabs',
   templateUrl: './stats-tabs.component.html',
   styleUrls: ['./stats-tabs.component.css']
 })
-export class StatsTabsComponent implements OnInit, OnDestroy {
-  private ssRoute?: Subscription;
-  selectedTab = 0;
-
-  constructor(
-    private route: ActivatedRoute,
-  ) {
-  }
-
-  ngOnInit(): void {
-    this.ssRoute = this.route.url.subscribe(urls => {
-      switch (urls[1].path) {
-        case 'players':
-          this.selectedTab = 1;
-          break;
-        case 'difficulty':
-        default:
-          this.selectedTab = 0;
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.ssRoute?.unsubscribe();
-  }
+export class StatsTabsComponent {
 }
