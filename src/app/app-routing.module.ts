6b10c57aeba51b2ec857f9f4bd23067d8883b0fd
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2023-12-22 16:14:38 +0900

    ng update @angular/core@15 @angular/cli@15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -268,9 +268,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    relativeLinkResolution: 'legacy',
     scrollPositionRestoration: 'enabled'
-  })],
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
