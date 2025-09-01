92874163f1bb53c762edbae1ae3063a4fda0d5c0
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2025-09-01 09:08:05 +0900

    feat: サーチフィルタを追加実装

--- src/app/cash-table/cash-table-datasource.ts
+++ src/app/cash-table/cash-table-datasource.ts
@@ -1,9 +1,9 @@
 import { inject } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { BehaviorSubject } from 'rxjs';
-import { finalize } from 'rxjs/operators';
 
 import { RemoteService } from '../remote.service';
+import { LocalizeService } from '../localize.service';
 import { EventInfo } from '../models';
 
 /**
@@ -13,23 +13,28 @@ import { EventInfo } from '../models';
  */
 export class CashTableDataSource extends MatTableDataSource<EventInfo> {
   private remote = inject(RemoteService);
+  private localize = inject(LocalizeService);
   loading = true;
 
+  constructor() {
+    super();
+    this.setupFilter();
+  }
+  
   /**
    * Connect this data source to the table. The table will only update when
    * the returned stream emits new items.
    * @returns A stream of the items to be rendered.
    */
   override connect(): BehaviorSubject<EventInfo[]> {
+    this.loading = true;
     this.remote.getCashOfEvents()
-      .pipe(
-        finalize(() => { this.loading = false; })
-      )
       .subscribe({
         next: events => {
           this.data = events;
         },
-        error: err => console.error(err)
+        error: err => console.error(err),
+        complete: () => { this.loading = false; }
       });
     return super.connect();
   }
@@ -39,4 +44,33 @@ export class CashTableDataSource extends MatTableDataSource<EventInfo> {
    * any open connections or free any held resources that were set up during connect.
    */
   override disconnect(): void {}
+
+  private setupFilter() {
+    this.filterPredicate = (data: EventInfo, filters: string): boolean => {
+      const matchFilter: boolean[] = [];
+      const filterArray = filters.split('&');
+      const columns: string[] = [];
+
+      if (data.title) {
+        columns.push(data.title);
+      }
+
+      if (data.title && this.localize) {
+        columns.push(this.localize.transform(data.title, 'event'));
+      }
+
+      if (data.period) {
+        columns.push(data.period.from);
+        columns.push(data.period.to);
+      }
+
+      filterArray.forEach(filter => {
+        const customFilter: boolean[] = [];
+        columns.forEach(column =>
+          customFilter.push(column.toLowerCase().includes(filter)));
+        matchFilter.push(customFilter.some(Boolean)); // OR
+      });
+      return matchFilter.every(Boolean); // AND
+    };
+  }
 }
