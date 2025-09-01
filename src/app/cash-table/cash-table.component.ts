b44f371edbe2daab1d647ffff3fecb903863410f
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2025-09-01 09:08:27 +0900

    feat: 検索機能を追加し、ページサイズオプションを動的に設定

--- src/app/cash-table/cash-table.component.ts
+++ src/app/cash-table/cash-table.component.ts
@@ -1,9 +1,13 @@
-import { AfterViewInit, Component, ViewChild, inject } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, inject, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { CurrencyPipe, PercentPipe } from '@angular/common';
+import { CurrencyPipe, PercentPipe, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MatTableModule, MatTable } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { MatSortModule, MatSort } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { CashTableDataSource } from './cash-table-datasource';
 import { EventInfo } from '../models';
 import { LocalizePipe } from '../localize.pipe';
@@ -14,11 +18,15 @@ import { RoundThousandsPipe } from '../round-thousands.pipe';
   templateUrl: './cash-table.component.html',
   styleUrl: './cash-table.component.css',
   imports: [
+    CommonModule,
+    MatFormFieldModule, MatIconModule, FormsModule,　MatInputModule,
     MatTableModule, MatPaginatorModule, MatSortModule, CurrencyPipe, PercentPipe, LocalizePipe, RoundThousandsPipe
   ]
 })
 export class CashTableComponent implements AfterViewInit {
   private router = inject(Router);
+  @Input() limit?: number;
+  @Input() search = '';
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<EventInfo>;
@@ -26,6 +34,10 @@ export class CashTableComponent implements AfterViewInit {
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['title', 'purse', 'payout', 'cashing'];
+  pageSizeOptions = [30, 60, 120];
+
+  get loading(): boolean { return this.dataSource.loading; }
+  get isMinimum(): boolean { return !!this.limit && this.limit <= this.pageSizeOptions[0]; }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
@@ -49,5 +61,13 @@ export class CashTableComponent implements AfterViewInit {
 
   onRawClicked(event: EventInfo): void {
     this.router.navigate(['/event', event.id]);
+  }
+
+  applyFilter(filterValue: string): void {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+    this.search = filterValue;
   }
 }
